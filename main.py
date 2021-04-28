6e65622ef058fda104a06c59b0deaa1af568bc5c
Ilia Egorov <icegorov@gmail.com>
2021-04-28 19:49:03 -0400

    Included atomic mass for each element

--- main.py
+++ main.py
@@ -1,32 +1,67 @@
 from tkinter import *
-from periodictable import elements
+from tkinter import ttk
+import periodictable
 
-# Create & Configure window 
-window = Tk()
-window.geometry('800x600')
+# Global variables
+s_orbital = 2
+p_orbital = 6
+d_orbital = 10
+f_orbital = 14
 
-# Creating the square frame
-square_frame = Frame(window, width=300, height=300)
-square_frame.grid(column=0, row=0)
-square_frame.grid_propagate(False)
-square_frame.rowconfigure(0, weight=1)
-square_frame.columnconfigure(0, weight=1)
+grid_col = 18
 
-# Setting Up Grid
-Grid.rowconfigure(window, 0, weight=1)
-Grid.columnconfigure(window, 0, weight=1)
+Be_index = 19
+Mg_index = 37
 
-# Create a 5x10 (rows x columns) grid of buttons inside the frame
-for row_index in range(5):
-    Grid.rowconfigure(window, row_index, weight=1)
-    for col_index in range(10):
-        Grid.columnconfigure(window, col_index, weight=1)
-        block = []
-        
-        block.append(elements[(row_index * 10) + col_index + 1].symbol)
-        block.append(str((row_index * 10) + col_index + 1))
 
-        atomic_number = Label(window, text="\n\n".join(block), height="70", width="70")
-        atomic_number.grid(row=row_index, column=col_index, sticky="NESW")  
+class Element:
+    def __init__(self, name, symbol, atomic_num, atomic_mass):
+        self.name = name
+        self.symbol = symbol
+        self.atomic_num = atomic_num
+        self.atomic_mass = atomic_mass
 
-window.mainloop()
+    def add_block(self, mainframe, block, grid_index):
+        row_index = grid_index // grid_col
+        col_index = grid_index % grid_col
+        Label(mainframe, text="\n".join([str(e) for e in block]), height=8, width=8).grid(
+              column=col_index, row=row_index, sticky="NESW")
+
+
+# Setup Tkinter frame
+root = Tk()
+root.title("Periodic Table")
+
+mainframe = ttk.Frame(root)
+mainframe.grid(column=0, row=0, sticky="NESW")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+# Add all elements from 'periodictable' library to an array
+elements = []
+for el in periodictable.elements:
+    elements.append(el.name)
+
+# Get elements' properties from library and display it on the table
+grid_index = 0
+elements_len = len(elements)
+block = []
+for i in range(1, elements_len):
+    block.append(i)
+    block.append(periodictable.elements[i])
+    block.append("{:.3f}".format(periodictable.elements[i].mass))
+
+    atom = Element(periodictable.elements[i].name, periodictable.elements[i], 
+                    grid_index, periodictable.elements[i].mass)
+    atom.add_block(mainframe, block, grid_index)
+
+    block.clear()
+
+    # Space out each element block appropriately by incrementing grid_index
+    if (grid_index == 0):
+        grid_index += p_orbital + d_orbital
+    elif (grid_index == Be_index or grid_index == Mg_index):
+        grid_index += d_orbital
+    grid_index += 1
+
+root.mainloop()
