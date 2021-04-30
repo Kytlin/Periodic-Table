5a46e8e5284ccdfac9205ffc5ae5fc4a888d2529
Ilia Egorov <icegorov@gmail.com>
2021-04-29 22:02:16 -0400

    Added text to window

--- main.py
+++ main.py
@@ -13,27 +13,32 @@ grid_col = 18
 Be_index = 19
 Mg_index = 37
 
+def more_info(blocks, i):
+    top = Toplevel()
+    top.title("More Info")
 
-class Element:
-    def __init__(self, name, symbol, atomic_num, atomic_mass):
-        self.name = name
-        self.symbol = symbol
-        self.atomic_num = atomic_num
-        self.atomic_mass = atomic_mass
+    print(i)
+    print(blocks[i]["name"])
 
-    def add_block(self, mainframe, block, grid_index):
-        row_index = grid_index // grid_col
-        col_index = grid_index % grid_col
-        Label(mainframe, text="\n".join([str(e) for e in block]), height=8, width=8).grid(
-              column=col_index, row=row_index, sticky="NESW")
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    top.columnconfigure(0, weight=1)
+    top.rowconfigure(0, weight=1)
+
+    labelExample = Label(top, text="{} ({})".format(blocks[i]["name"], blocks[i]["symbol"]), height=3, width=30).grid(column=0, row=0)
+
+def add_block(mainframe, blocks, block, grid_index, atomic_num):
+    row_index = grid_index // grid_col
+    col_index = grid_index % grid_col
+    element = Button(mainframe, text="\n".join([str(e) for e in block]), height=8, width=8, command=lambda: more_info(blocks, atomic_num)).grid(
+            column=col_index, row=row_index, sticky=(N, W, E, S))
 
 
 # Setup Tkinter frame
 root = Tk()
 root.title("Periodic Table")
 
-mainframe = ttk.Frame(root)
-mainframe.grid(column=0, row=0, sticky="NESW")
+mainframe = Frame(root)
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
@@ -46,14 +51,19 @@ for el in periodictable.elements:
 grid_index = 0
 elements_len = len(elements)
 block = []
+blocks = []
 for i in range(1, elements_len):
     block.append(i)
     block.append(periodictable.elements[i])
     block.append("{:.3f}".format(periodictable.elements[i].mass))
 
-    atom = Element(periodictable.elements[i].name, periodictable.elements[i], 
-                    grid_index, periodictable.elements[i].mass)
-    atom.add_block(mainframe, block, grid_index)
+    properties = {"atomic_num": i, "name": periodictable.elements[i - 1].name, 
+             "symbol": periodictable.elements[i - 1], "mass": "{:.3f}".format(periodictable.elements[i - 1].mass)}
+    blocks.append(properties)
+
+    # atom = Element(periodictable.elements[i].name, periodictable.elements[i], 
+    #                grid_index, periodictable.elements[i].mass)
+    add_block(mainframe, blocks, block, grid_index, i)
 
     block.clear()
 
